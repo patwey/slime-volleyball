a0f65235ab17c5d59f453d56ac1d254af6e11015
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-07 12:41:15 -0700

    Add slime ability to move left and right

--- slime.js
+++ slime.js
@@ -2,10 +2,49 @@ $(document).ready(function () {
   canvas = document.getElementById("game");
   context = canvas.getContext("2d");
 
-  function Slime(x, y, color) {
+  function Slime(x, y, color, moveLeftKeyCode, moveRightKeyCode) {
     this.x = x;
     this.y = y;
     this.color = color;
+
+    this.moveLeftKeyCode = moveLeftKeyCode;
+    this.moveRightKeyCode = moveRightKeyCode;
+
+    this.isMoveLeftKeyPressed = false;
+    this.isMoveRightKeyPressed = false;
+
+    createEventListener(this);
+  }
+
+  function createEventListener(slime) {
+    window.addEventListener("keydown", onKeyDown.bind(slime), false);
+    window.addEventListener("keyup", onKeyUp.bind(slime), false);
+  }
+
+  function onKeyDown(e) {
+    var keyPressed = e.code;
+
+    switch (keyPressed) {
+      case this.moveLeftKeyCode:
+        this.isMoveLeftKeyPressed = true;
+        break;
+      case this.moveRightKeyCode:
+        this.isMoveRightKeyPressed = true;
+        break;
+    }
+  }
+
+  function onKeyUp(e) {
+    var keyReleased = e.code;
+
+    switch (keyReleased) {
+      case this.moveLeftKeyCode:
+        this.isMoveLeftKeyPressed = false;
+        break;
+      case this.moveRightKeyCode:
+        this.isMoveRightKeyPressed = false;
+        break;
+    }
   }
 
   Slime.prototype.drawBody = function () {
@@ -39,12 +78,22 @@ $(document).ready(function () {
     this.drawEye();
   }
 
-  var slimePat = new Slime(375, 375, "pink")
+  Slime.prototype.move = function () {
+    if (this.isMoveRightKeyPressed) {
+      this.x++;
+    } else if (this.isMoveLeftKeyPressed) {
+      this.x--;
+    }
+
+    return this;
+  }
+
+  var slimePat = new Slime(375, 375, "pink", "KeyA", "KeyD")
 
   requestAnimationFrame(function gameLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    slimePat.draw();
+    slimePat.move().draw();
 
     requestAnimationFrame(gameLoop);
   });
