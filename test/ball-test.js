9846dd8fe60741bb14153233c44af6f896149923
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-10 11:56:10 -0700

    Bounce ball of wall
    - Prevent ball from leaving canvas on sides
    - Add tests for ball bouncing off wall
    - Add canvas to Ball constructor
    - Add context param to Ball.draw()

--- test/ball-test.js
+++ test/ball-test.js
@@ -2,9 +2,49 @@ var assert = require('chai').assert;
 var Ball   = require('../lib/ball');
 
 describe("Ball", function() {
-  it("has a radius of 12", function() {
-    var ball = new Ball(0, 0);
+  beforeEach(function() {
+    this.canvas = { width: 750, height: 375 };
+    this.ball = new Ball(0, 0, this.canvas);
+    this.ball.dx = 10;
+  });
 
-    assert.equal(ball.radius, 12);
+  function moveBallToTouchRightWall(ball) {
+    ball.x = (ball.canvas.width - ball.radius);
+  };
+
+  function moveBallToTouchLeftWall(ball) {
+    ball.x = (0 + ball.radius);
+  };
+
+  it("has a radius of 12", function() {
+    assert.equal(this.ball.radius, 12);
+  });
+
+  context("it hits a wall", function() {
+    it("knows if it hits a wall", function() {
+      moveBallToTouchRightWall(this.ball);
+
+      assert.isTrue(this.ball.isTouchingWall());
+
+      moveBallToTouchLeftWall(this.ball);
+
+      assert.isTrue(this.ball.isTouchingWall());
+    });
+
+    it("reverses horizontal direction", function() {
+      var oldDx = this.ball.dx;
+
+      moveBallToTouchRightWall(this.ball);
+      this.ball.move();
+
+      assert.strictEqual(this.ball.dx, -oldDx);
+
+      oldDx = this.ball.dx;
+
+      moveBallToTouchLeftWall(this.ball);
+      this.ball.move();
+
+      assert.strictEqual(this.ball.dx, -oldDx);
+    })
   });
 });
