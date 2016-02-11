6510ceef842641b698790a03fc672a82e567fab4
Pat Wey <patrickwey@gmail.com>
2016-02-11 12:53:08 -0700

    Update file structure to split stylesheets and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./lib/index.js",
+    main: "./lib/scripts/index.js",
     test: "mocha!./test/index.js"
   },
   output: {
