861ede5cd7e439d6f17f18a99ca72ac1082d2488
Pat Wey <patrickwey@gmail.com>
2016-02-13 15:34:39 -0700

    Finish adding second player
    * Draw left and right slime eyes differently
    * Confine left and right slimes to their side of the net
    * Update testing

--- test/slime-test.js
+++ test/slime-test.js
@@ -1,58 +1,111 @@
 var assert = require('chai').assert;
 var Slime  = require('../lib/scripts/slime');
+var Net    = require('../lib/scripts/net');
 
 describe("Slime", function() {
+  function moveSlimeAwayFromWalls(slime) {
+    slime.x = slime.canvas.width / 4;
+  }
+
   beforeEach(function() {
     var canvas = { width: 750, height: 375 };
-    this.slime = new Slime(100, 300, "blue", "keyA", "keyD", "", canvas);
+    this.slime = new Slime(100, 375, "blue", "keyA", "keyD", "", canvas, true);
+    this.net   = new Net(canvas.width, canvas.height);
   });
 
   it("it knows its left edge", function() {
-    var expectedEdge = this.slime.x - this.slime.radius
+    var expectedEdge = this.slime.x - this.slime.radius;
     assert.strictEqual(this.slime.leftEdge(), expectedEdge);
   });
 
   it("it knows its right edge", function() {
-    var expectedEdge = this.slime.x + this.slime.radius
+    var expectedEdge = this.slime.x + this.slime.radius;
     assert.strictEqual(this.slime.rightEdge(), expectedEdge);
   });
 
-  context("it knows if its touching the wall", function() {
+  context("it is a slime on the left side", function() {
     function moveSlimeToLeftWall(slime) {
       slime.x = 0 + slime.radius;
     }
 
+    function moveSlimeToLeftCenter(slime) {
+      slime.x = slime.canvas / 4;
+    }
+
+    function moveSLimeToTouchLeftSideOfNet(slime, net) {
+      slime.x = net.x - slime.radius;
+    }
+
+    beforeEach(function () {
+      var canvas = { width: 750, height: 375 };
+      this.leftSlime = new Slime(100, 375, "blue", "keyA", "keyD", "", canvas, true)
+    })
+
+    it("knows if its touching the left wall", function() {
+      moveSlimeToLeftCenter(this.leftSlime);
+
+      assert.isFalse(this.leftSlime.isTouchingLeftWall());
+
+      moveSlimeToLeftWall(this.leftSlime);
+
+      assert.isTrue(this.leftSlime.isTouchingLeftWall());
+    });
+
+    it("knows if it's touching the left side of the net", function() {
+      moveSlimeToLeftCenter(this.leftSlime);
+
+      assert.isFalse(this.leftSlime.isTouchingLeftSideOfNet());
+
+      moveSLimeToTouchLeftSideOfNet(this.leftSlime, this.net);
+
+      assert.isTrue(this.leftSlime.isTouchingLeftSideOfNet());
+    });
+  });
+
+  context("it is a slime on the right side", function() {
     function moveSlimeToRightWall(slime) {
       slime.x = slime.canvas.width - slime.radius;
     }
 
-    function moveSlimeAwayFromWalls(slime) {
-      slime.x = slime.canvas / 2;
+    function moveSlimeToRightCenter(slime) {
+      slime.x = slime.canvas.width - (slime.canvas.width / 4);
     }
 
-    it("it knows if its touching the left wall", function() {
-      moveSlimeAwayFromWalls(this.slime);
+    function moveSlimeToTouchRightSideOfNet(slime, net) {
+      slime.x = net.x + net.width + slime.radius;
+    }
 
-      assert.isFalse(this.slime.isTouchingLeftWall());
+    beforeEach(function () {
+      var canvas = { width: 750, height: 375 };
+      this.rightSlime = new Slime(600, 375, "blue", "keyA", "keyD", "", canvas, false)
+    })
 
-      moveSlimeToLeftWall(this.slime);
+    it("knows if its touching the right wall", function() {
+      moveSlimeToRightCenter(this.rightSlime)
 
-      assert.isTrue(this.slime.isTouchingLeftWall());
+      assert.isFalse(this.rightSlime.isTouchingRightWall());
+
+      moveSlimeToRightWall(this.rightSlime);
+
+      assert.isTrue(this.rightSlime.isTouchingRightWall());
     });
 
-    it("it knows if its touching the right wall", function() {
-      moveSlimeAwayFromWalls(this.slime);
+    it("knows if it's touching the right side of the net", function() {
+      moveSlimeToRightCenter(this.rightSlime);
 
-      assert.isFalse(this.slime.isTouchingRightWall());
+      assert.isFalse(this.rightSlime.isTouchingRightSideOfNet());
 
-      moveSlimeToRightWall(this.slime);
+      moveSlimeToTouchRightSideOfNet(this.rightSlime, this.net);
 
-      assert.isTrue(this.slime.isTouchingRightWall());
+      assert.isTrue(this.rightSlime.isTouchingRightSideOfNet());
     });
   });
 
   context("it can move", function() {
     it("moves left when left key pressed", function() {
+      moveSlimeAwayFromWalls(this.slime);
+      debugger
+
       var oldX = this.slime.x;
       this.slime.move();
 
