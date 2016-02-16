40d11a0f13d343f09b0359b69e50961ca005c92f
Pat Wey <patrickwey@gmail.com>
2016-02-16 14:59:33 -0700

    Run npm build

--- test.bundle.js
+++ test.bundle.js
@@ -728,8 +728,8 @@
 	// Hot Module Replacement
 	if(false) {
 		// When the styles change, update the <style> tags
-		module.hot.accept("!!/Users/Torie/Documents/turing/module_4/slime-volleyball/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/Torie/Documents/turing/module_4/slime-volleyball/node_modules/mocha/mocha.css", function() {
-			var newContent = require("!!/Users/Torie/Documents/turing/module_4/slime-volleyball/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/Torie/Documents/turing/module_4/slime-volleyball/node_modules/mocha/mocha.css");
+		module.hot.accept("!!/Users/patwey/code/slime_volleyball/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/patwey/code/slime_volleyball/node_modules/mocha/mocha.css", function() {
+			var newContent = require("!!/Users/patwey/code/slime_volleyball/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/patwey/code/slime_volleyball/node_modules/mocha/mocha.css");
 			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 			update(newContent);
 		});
